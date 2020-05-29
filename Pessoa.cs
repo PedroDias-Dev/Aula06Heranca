437e8389b0e89281f725d085a17dd49087597202
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-05-29 15:49:45 -0300

    Herança Finalizada

---
+++ Pessoa.cs
@@ -0,0 +1,13 @@
+namespace Herança
+{
+    public class Pessoa
+    {
+        public string nome;
+
+        public string Saudar(){
+            return "Seja bem vindo/a, " +nome+ ".";
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPF cpf = new CPF();
+
+            cpf.nome = "Pedro";
+
+            Console.WriteLine(cpf.Saudar());
+
+            cpf.cpf = "000.000.000-00";
+            Console.WriteLine(cpf.ValidarCPF());
+        }
+
+    }
+}
